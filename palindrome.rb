49c6252e20911f564a28371e6d3683073a50066a
David Souza <dvdcmd@gmail.com>
2016-11-09 09:13:34 -0200

    Hashes and symbols

--- palindrome.rb
+++ palindrome.rb
@@ -1,9 +1,3 @@
-#!/usr/bin/ruby
-# @Author: David Souza
-# @Date:   2016-10-27 08:51:13
-# @Last Modified by:   David Souza
-# @Last Modified time: 2016-10-27 08:53:18
-
 def palindrome_tester(s)
     if s == s.reverse
         puts "it's a palindrome!"
