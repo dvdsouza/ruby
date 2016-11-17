72add6803c379e06dab9d37199a2ac7b47da1e22
David Souza <dvdcmd@gmail.com>
2016-11-17 08:52:48 -0200

    modifying built-in class

--- oop.rb
+++ oop.rb
@@ -1,9 +1,36 @@
-literal_c = 1..10
-literal_c.each { |c| puts c }
-
-named = Range.new(1,10)
-named.each do |c|
-    puts c
+class Word < String
+    def palindrome?
+        self == reverse
+    end
 end
 
-puts literal_c == named
+s = Word.new("level")
+puts s.palindrome?
+puts s.length
+puts s.class
+puts s.class.superclass
+puts s.class.superclass.superclass
+
+range = Range.new(1,10)
+puts range.class
+puts range.class.superclass
+puts range.class.superclass.superclass
+
+hash = Hash.new {}
+puts hash.class
+puts hash.class.superclass
+puts hash.class.superclass.superclass
+
+symbol = :symbol1
+puts symbol.class
+puts symbol.class.superclass
+puts symbol.class.superclass.superclass
+
+
+class String
+    def palindrome?
+        self == self.reverse
+    end
+end
+
+puts "deified".palindrome?
